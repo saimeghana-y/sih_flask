f810f334272c020dd1e57fb1d4d0b8bc7720df92
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2022-05-08 23:22:39 +0530

    added nav-bar

--- sentiment/main.py
+++ sentiment/main.py
@@ -2,11 +2,14 @@ from flask import Flask, request, render_template,jsonify
 from textblob import TextBlob
 
 app = Flask(__name__)
+
 def do_something(text1):
    return TextBlob(text1).sentiment.polarity
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 @app.route('/join', methods=['GET','POST'])
 def my_form_post():
     text1 = request.form['text1']
@@ -16,5 +19,6 @@ def my_form_post():
     }
     result = {str(key): value for key, value in result.items()}
     return jsonify(result=result)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
